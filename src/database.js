e1406245cb7765c211d9f497a4ed7919bd0cb621
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-01-31 22:46:52 -0300

    Database insert and route create a new task

---
+++ src/database.js
@@ -0,0 +1,33 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../db.json", import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database, null, 2));
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+
+    return data;
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
 import { buildRoutePath } from "./utils/build-route-path.js";
+
+const database = new Database();
 
 export const routes = [
   // Create a new task
@@ -8,7 +12,30 @@ export const routes = [
     handler: (req, res) => {
       const { title, description } = req.body;
 
-      return res.writeHead(201).end(JSON.stringify({ title, description }));
+      if (!title) {
+        return res
+          .writeHead(400)
+          .end(JSON.stringify({ message: "Title is required." }));
+      }
+
+      if (!description) {
+        return res
+          .writeHead(400)
+          .end(JSON.stringify({ message: "Description is required." }));
+      }
+
+      const task = database.insert("tasks", {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+
+      return res
+        .writeHead(201)
+        .end(JSON.stringify({ message: "Task created successfully.", task }));
     },
   },
 ];
