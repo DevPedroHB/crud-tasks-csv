d12e66b07c8ffd3aca59c950391fabbc5e9d540d
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-01-31 22:58:46 -0300

    Stream import CSV

---
+++ streams/import-csv.js
@@ -0,0 +1,38 @@
+import { parse } from "csv-parse";
+import fs from "node:fs";
+
+const csvPath = new URL("./tasks.csv", import.meta.url);
+const stream = fs.createReadStream(csvPath);
+
+const csvParse = parse({
+  delimiter: ",",
+  skipEmptyLines: true,
+  fromLine: 2,
+});
+
+async function run() {
+  const linesParse = stream.pipe(csvParse);
+
+  for await (const line of linesParse) {
+    const [title, description] = line;
+
+    await fetch("http://localhost:3333/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+      }),
+    });
+
+    await wait(1000);
+  }
+}
+
+run();
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
